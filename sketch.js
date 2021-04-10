cebd662a79741c1787d70a7869520d6efe5eb7f8
Aakash03122005 <69099671+Aakash03122005@users.noreply.github.com>
2021-04-10 21:47:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,95 @@
+//create feed and lastFed variable here
+var feed,lastFed;
+// variables for the sprites.
+var dog,sadDog,happyDog, database;
+var foodS,foodStock,fedTime;
+var addFood;
+var foodObj;
+
+// preload function for uploading images..
+function preload(){
+sadDog=loadImage("Dog.png");
+happyDog=loadImage("happy dog.png");
+}
+// setup function for making sprites.
+function setup() {
+  database=firebase.database();
+  createCanvas(1000,400);
+
+  foodObj = new Food();
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+  //create feed the dog button here
+  addFood=createButton("Feed the Dog");
+  addFood.position(700,95);
+  addFood.mousePressed(feedDog);
+
+  addFood=createButton("Add Food");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
+
+}
+// draw function for displaying the sprites.
+function draw() {
+  background(46,139,87);
+  foodObj.display();
+
+  //write code to read fedtime value from the database 
+  fedTime=database.ref('FeedTime');
+  fedTime.on("value",function(data){
+    lastFed=data.val();
+  });
+ 
+  //write code to display text lastFed time here
+  textSize(24)
+  fill("black")
+  text("Hi,there I am Your Puppy",650,80)
+  text("Coco!!",750,300)
+ 
+  fill("black");
+  textSize(15);
+  if(lastFed>=12){
+    text("Last Feed : "+ lastFed%12 + " PM", 300,50);
+   }else if(lastFed==0){
+     text("Last Feed : 12 AM",350,30);
+   }else{
+     text("Last Feed : "+ lastFed + " AM", 300,50);
+   }
+
+  drawSprites();
+}
+
+//function to read food Stock
+function readStock(data){
+  foodS=data.val();
+  foodObj.updateFoodStock(foodS);
+}
+// function to feed the dog.
+function feedDog(){
+  dog.addImage(happyDog);
+  //write code here to update food stock and last fed time
+  if(foodObj.getFoodStock()<= 0){
+    foodObj.updateFoodStock(foodObj.getFoodStock()*0);
+  }else{
+    foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  }
+  
+   database.ref('/').update({
+                            Food:foodObj.getFoodStock(),
+                            FeedTime:hour()
+                            })
+}
+
+//function to add food in stock
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+  Food:foodS
+  })
+}
